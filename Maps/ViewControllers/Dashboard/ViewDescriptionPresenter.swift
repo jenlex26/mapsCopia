32b4cca81c10ef17ffbaa3ae2a4f3bf3bc428a88
ALBERTO GONZALEZ <albertogonzalez@ALBERTOs-MacBook-Pro.local>
2019-09-03 12:19:11 -0500

    fix

--- Maps/ViewControllers/Dashboard/ViewDescriptionPresenter.swift
+++ Maps/ViewControllers/Dashboard/ViewDescriptionPresenter.swift
@@ -8,22 +8,70 @@
 
 import Foundation
 import UIKit
-import PDFKit
+import MapKit
+import AlamofireImage
+import Alamofire
+
+
 
 protocol DescriptionView {
     func showProgress()
     func hideProgress()
+    func getContry(annotation: MKPointAnnotation, region: MKCoordinateRegion)
+    func currentError(err:String)
+    func getImage(image: UIImage)
 }
 
 class DescriptionViewPresenter {
     
     var view: DescriptionView
-    var viewController: ViewDescriptionVC
     
-    
-    init(view: DescriptionView, vc: ViewDescriptionVC) {
+    init(view: DescriptionView) {
         self.view = view
-        viewController = vc
+    }
+    
+    func search(name: String) {
+        let search = MKLocalSearch.Request()
+        search.naturalLanguageQuery = name
+        
+        let activeSearch = MKLocalSearch(request: search)
+        
+        activeSearch.start { (response, error) in
+            if response == nil {
+                print("nada")
+                self.view.currentError(err: "No se encontro\(name)")
+            }else{
+                let latitud = response?.boundingRegion.center.latitude
+                let longitud = response?.boundingRegion.center.longitude
+                
+                let annotation = MKPointAnnotation()
+                annotation.title = name
+                annotation.coordinate = CLLocationCoordinate2DMake(latitud!, longitud!)
+                
+                
+                
+                let coordenate:CLLocationCoordinate2D = CLLocationCoordinate2DMake(latitud!, longitud!)
+                let span = MKCoordinateSpan(latitudeDelta: 30,longitudeDelta: 30)
+                let region = MKCoordinateRegion(center: coordenate, span: span)
+//                self.maps.setRegion(region, animated: true)
+                // Pasando el annotation al viewcontroller
+                self.view.getContry(annotation: annotation, region: region)
+            }
+        }
+    }
+    
+    func imageData(urlData: String) {
+        let imageUrl = urlData
+        Alamofire.request(imageUrl).downloadProgress(closure: {
+            (progrees) in
+        }).responseData {(response) in
+            if let data = response.result.value {
+                DispatchQueue.main.async {
+                    let image = UIImage(data: data)
+                    self.view.getImage(image: image ?? UIImage())
+                }
+            }
+        }
     }
 
 }
